2d66aceaab41322cd338355926dc1f75c85bf741
Mehradi <mehradi.info@gmail.com>
2023-12-12 16:06:51 +0100

    Get element by tag name, attribute and value, id, class, multiple classes, tow diffrent attribute and xpath

--- cypress/e2e/webDriverUni/contact-us.cy.ts
+++ cypress/e2e/webDriverUni/contact-us.cy.ts
@@ -1,4 +1,5 @@
 /// <reference types="cypress"/>
+/// <reference types="cypress-xpath"/>
 
 describe("contact-us form", () => {
   beforeEach(() => {
@@ -17,5 +18,25 @@ describe("contact-us form", () => {
     cy.get('[name="last_name"]').type("blogs");
     cy.get("textarea.feedback-input").type("How do you It?");
     cy.get('[type="submit"]').click();
+  });
+  it.only("Get element by tag name, attribute and value, id, class, multiple classes, tow diffrent attribute and xpath", () => {
+    // By tag name
+    cy.get("textarea").type("By tag name");
+    // By attribute and value
+    cy.get("input[name='first_name']").type("By attribute and value");
+    // By id
+    cy.get("#contact_me").click();
+    // By class
+    cy.get(".navbar-header").then((e) => e.remove());
+    // By multiple classes
+    cy.get("[class='navbar navbar-inverse navbar-fixed-top']").then((e) =>
+      e.remove()
+    );
+    // By tow diffrent attribute
+    cy.get("[name='email'][placeholder='Email Address']").type(
+      "By tow diffrent attribute"
+    );
+    // By xpath
+    cy.xpath("//input[@name='first_name']").type("By xpath");
   });
 });
