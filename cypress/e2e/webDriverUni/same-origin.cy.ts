1c9f9822ed4b6f805dfe613eb64772e6417e84fe
Mehradi <mehradi.info@gmail.com>
2023-12-16 19:25:31 +0100

    Validate visiting two diffrent domains via user actions && Origin command

---
+++ cypress/e2e/webDriverUni/same-origin.cy.ts
@@ -0,0 +1,20 @@
+/// <reference types="cypress"/>
+/// <reference types="cypress-xpath"/>
+
+describe("contact-us form", () => {
+  beforeEach(() => {});
+
+  it("Validate visiting two diffrent domains via user actions", () => {
+    cy.visit("https://www.webdriveruniversity.com");
+    cy.get("#automation-test-store").invoke("removeAttr", "target").click();
+  });
+  it.only("Origin command", () => {
+    cy.origin("https://www.bing.com", () => {
+      cy.visit("/");
+    });
+
+    cy.origin("https://www.google.com", () => {
+      cy.visit("/");
+    });
+  });
+});
