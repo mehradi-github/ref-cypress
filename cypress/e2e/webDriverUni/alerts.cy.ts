f2c761674d137bda9b62d783f35878f20473175e
Mehradi <mehradi.info@gmail.com>
2023-12-19 12:12:23 +0100

    Validate js confirm alert box works correctly when clicking ok

--- cypress/e2e/webDriverUni/alerts.cy.ts
+++ cypress/e2e/webDriverUni/alerts.cy.ts
@@ -12,4 +12,16 @@ describe("Handle js alerts", () => {
       expect(s).to.equal("I am an alert box!");
     });
   });
+  it.only("Validate js confirm alert box works correctly when clicking ok", () => {
+    cy.visit("https://www.webdriveruniversity.com");
+    cy.get("#popup-alerts")
+      .invoke("removeAttr", "target")
+      .click({ force: true });
+    cy.get("#button4").click();
+
+    cy.on("window:confirm", (s) => {
+      return true;
+    });
+    cy.get("#confirm-alert-text").contains("You pressed OK!");
+  });
 });
