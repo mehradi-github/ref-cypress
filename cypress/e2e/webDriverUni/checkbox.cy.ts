301a486b46399e626b8dc66b416e10e14e3798fa
Mehradi <mehradi.info@gmail.com>
2023-12-20 08:50:55 +0100

    Uncheck and validate checkbox

--- cypress/e2e/webDriverUni/checkbox.cy.ts
+++ cypress/e2e/webDriverUni/checkbox.cy.ts
@@ -11,4 +11,9 @@ describe("Verify checkboxes via webdriveruni", () => {
     cy.get("#checkboxes > :nth-child(1) > input").as("option-1");
     cy.get("@option-1").check().should("be.checked");
   });
+
+  it("Uncheck and validate checkbox", () => {
+    cy.get(":nth-child(5) > input").as("option-3");
+    cy.get("@option-3").uncheck().should("not.be.checked");
+  });
 });
