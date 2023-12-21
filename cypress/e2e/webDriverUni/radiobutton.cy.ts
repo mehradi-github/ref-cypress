979545cfe731ad446c8606cca9ca5553d7db1ebe
Mehradi <mehradi.info@gmail.com>
2023-12-21 08:21:55 +0100

    Validate the states of specific radio buttons

--- cypress/e2e/webDriverUni/radiobutton.cy.ts
+++ cypress/e2e/webDriverUni/radiobutton.cy.ts
@@ -11,4 +11,17 @@ describe("Verify radio buttons via webdriveruni", () => {
     cy.get("#radio-buttons").find("[type='radio']").first().check();
     cy.get("#radio-buttons").find("[type='radio']").eq(1).check();
   });
+  it.only("Validate the states of specific radio buttons", () => {
+    cy.get("[value='lettuce']").as("lettuce");
+    cy.get("[value='pumpkin']").as("pumpkin");
+
+    cy.get("@lettuce").should("not.be.checked");
+    cy.get("@pumpkin").should("be.checked");
+
+    cy.get("@lettuce").check();
+
+    cy.get("@lettuce").should("be.checked");
+    cy.get("@pumpkin").should("not.be.checked");
+    cy.get("[value='cabbage'").should("be.disabled");
+  });
 });
