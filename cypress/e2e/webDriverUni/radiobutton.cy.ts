aade5be7bd0f58804d31bb275d1607247f724b34
Mehradi <mehradi.info@gmail.com>
2023-12-21 08:16:23 +0100

    Check specific radio buttons

---
+++ cypress/e2e/webDriverUni/radiobutton.cy.ts
@@ -0,0 +1,14 @@
+/// <reference types="cypress" />
+
+describe("Verify radio buttons via webdriveruni", () => {
+  beforeEach(() => {
+    cy.visit("https://www.webdriveruniversity.com");
+    cy.get("#dropdown-checkboxes-radiobuttons")
+      .invoke("removeAttr", "target")
+      .click({ force: true });
+  });
+  it("Check specific radio buttons", () => {
+    cy.get("#radio-buttons").find("[type='radio']").first().check();
+    cy.get("#radio-buttons").find("[type='radio']").eq(1).check();
+  });
+});
