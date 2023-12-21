2156d67e898c24fe2d55783b5853064042f9dba2
Mehradi <mehradi.info@gmail.com>
2023-12-21 08:38:52 +0100

    Select specific values via select dropdown lists

--- cypress/e2e/webDriverUni/radiobutton-dropdown.cy.ts
+++ cypress/e2e/webDriverUni/radiobutton-dropdown.cy.ts
@@ -11,7 +11,7 @@ describe("Verify radio buttons via webdriveruni", () => {
     cy.get("#radio-buttons").find("[type='radio']").first().check();
     cy.get("#radio-buttons").find("[type='radio']").eq(1).check();
   });
-  it.only("Validate the states of specific radio buttons", () => {
+  it("Validate the states of specific radio buttons", () => {
     cy.get("[value='lettuce']").as("lettuce");
     cy.get("[value='pumpkin']").as("pumpkin");
 
@@ -23,5 +23,11 @@ describe("Verify radio buttons via webdriveruni", () => {
     cy.get("@lettuce").should("be.checked");
     cy.get("@pumpkin").should("not.be.checked");
     cy.get("[value='cabbage'").should("be.disabled");
+  });
+
+  it.only("Select specific values via select dropdown lists", () => {
+    cy.get("#dropdowm-menu-1").select("c#");
+    cy.get("#dropdowm-menu-2").select("testng").should("have.value", "testng");
+    cy.get("#dropdowm-menu-3").select("JQuery").contains("JQuery");
   });
 });
