e88def42862e1976f3e0c8f5e8bb7c3c674a0e23
Mehradi <mehradi.info@gmail.com>
2023-12-23 08:28:33 +0100

    I should be able hold down the left mouse click button on a given element

--- cypress/e2e/webDriverUni/mouse-action.cy.ts
+++ cypress/e2e/webDriverUni/mouse-action.cy.ts
@@ -14,7 +14,14 @@ describe("Test mouse actions", () => {
       .trigger("mousemove")
       .trigger("mouseup", { force: true });
   });
-  it.only("I should be able to perform a double mouse click", () => {
+  it("I should be able to perform a double mouse click", () => {
     cy.get("#double-click").dblclick();
+  });
+  it.only("I should be able hold down the left mouse click button on a given element", () => {
+    cy.get("#click-box")
+      .trigger("mousedown", { which: 1 })
+      .then(($el) => {
+        expect($el).to.have.css("background-color", "rgb(0, 255, 0)");
+      });
   });
 });
