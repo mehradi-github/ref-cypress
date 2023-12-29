65344a35e8526a6b1941347c60fda06cc6fd6b34
Mehradi <mehradi.info@gmail.com>
2023-12-29 21:23:04 +0100

    Upload No file

--- cypress/e2e/webDriverUni/file-upload.cy.ts
+++ cypress/e2e/webDriverUni/file-upload.cy.ts
@@ -10,4 +10,11 @@ describe("Test File Upload via webdriveruni", () => {
     cy.get("#myFile").selectFile("cypress/fixtures/laptop.png");
     cy.get("#submit-button").click();
   });
+  it("Upload No file...", () => {
+    cy.visit("http://www.webdriveruniversity.com");
+    cy.get("#file-upload")
+      .invoke("removeAttr", "target")
+      .click({ force: true });
+    cy.get("#submit-button").click();
+  });
 });
