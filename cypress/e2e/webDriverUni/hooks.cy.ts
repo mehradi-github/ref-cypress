d4fa9045ce867baf9217769ddadc5760edad90c0
Mehradi <mehradi.info@gmail.com>
2023-12-30 20:43:56 +0100

    Check specific radio buttons

--- cypress/e2e/webDriverUni/hooks.cy.ts
+++ cypress/e2e/webDriverUni/hooks.cy.ts
@@ -2,15 +2,12 @@
 describe("Test File Upload via webdriveruni", () => {
   beforeEach(() => {
     cy.visit("https://www.webdriveruniversity.com");
-    cy.get("#file-upload")
+    cy.get("#dropdown-checkboxes-radiobuttons")
       .invoke("removeAttr", "target")
       .click({ force: true });
   });
-  it("Upload a file ...", () => {
-    cy.get("#myFile").selectFile("cypress/fixtures/laptop.png");
-    cy.get("#submit-button").click();
-  });
-  it("Upload No file...", () => {
-    cy.get("#submit-button").click();
+  it("Check specific radio buttons", () => {
+    cy.get("#radio-buttons").find("[type='radio']").first().check();
+    cy.get("#radio-buttons").find("[type='radio']").eq(1).check();
   });
 });
