d918bdedb775a91fbf74b4d91385b99e2f52677a
Mehradi <mehradi.info@gmail.com>
2024-01-01 09:38:24 +0100

    Check and validate checkbox

---
+++ cypress/e2e/webDriverUni/hooks-checkboxes.cy.ts
@@ -0,0 +1,13 @@
+/// <reference types="Cypress" />
+describe("Test File Upload via webdriveruni", () => {
+  beforeEach(() => {
+    cy.visit("https://www.webdriveruniversity.com");
+    cy.get("#dropdown-checkboxes-radiobuttons")
+      .invoke("removeAttr", "target")
+      .click({ force: true });
+  });
+  it("Check and validate checkbox", () => {
+    cy.get("#checkboxes > :nth-child(1) > input").as("option-1");
+    cy.get("@option-1").check().should("be.checked");
+  });
+});
