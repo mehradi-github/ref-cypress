ebfdf20f90019f44a3ab344041cb39eabb3a0d14
Mehradi <mehradi.info@gmail.com>
2024-01-03 19:10:46 +0100

    ContactForm_Submission

---
+++ cypress/e2e/webDriverUni/command-contact-us.cy.ts
@@ -0,0 +1,20 @@
+/// <reference types="Cypress" />
+describe("Test Contact Us form via Automation Test Store", () => {
+  before(() => {
+    cy.fixture("userDetails").as("user");
+  });
+  beforeEach(() => {
+    cy.visit("https://webdriveruniversity.com");
+    cy.get("#contact-us").invoke("removeAttr", "target").click({ force: true });
+  });
+  it("Should be able to submit a successful submission via contact us form", () => {
+    cy.get("@user").then((user) => {
+      cy.get('[name="first_name"]').type(user.first_name);
+      cy.get('[name="last_name"]').type(user.last_name);
+      cy.get('[name="email"]').type(user.email);
+    });
+    cy.get("textarea.feedback-input").type("How can I learn Cypress?");
+    cy.get('[type="submit"]').click();
+    cy.get("h1").should("have.text", "Thank You for your Message!");
+  });
+});
