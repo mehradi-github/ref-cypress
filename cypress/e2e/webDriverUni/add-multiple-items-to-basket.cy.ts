09ae5ac41a337f46ded4a7510cdfda6e4dd6c0c6
Mehradi <mehradi.info@gmail.com>
2024-01-04 09:43:46 +0100

    Adding test: add-multiple-items-to-basket.cy.ts

--- cypress/e2e/webDriverUni/add-multiple-items-to-basket.cy.ts
+++ cypress/e2e/webDriverUni/add-multiple-items-to-basket.cy.ts
@@ -6,4 +6,9 @@ describe("Add multiple items to basket", () => {
       globalThis.data = data;
     });
   });
+
+  beforeEach(() => {
+    cy.visit("https://automationteststore.com/");
+    cy.get("a[href*='product/category&path=']").contains("Hair Care").click();
+  });
 });
