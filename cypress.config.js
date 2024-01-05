d2275f01e2ea40c2c2e689492e5b2b684469e96c
Mehradi <mehradi.info@gmail.com>
2024-01-05 16:55:46 +0100

    Overriding Default Settings

--- cypress.config.js
+++ cypress.config.js
@@ -7,5 +7,15 @@ module.exports = defineConfig({
     },
     chromeWebSecurity: false,
     experimentalModifyObstructiveThirdPartyCode: true,
+
+    specPattern: "cypress/e2e/**/*.{js,jsx,ts,tsx,feature}",
+    excludeSpecPattern: [
+      "cypress/e2e/1-getting-started/*.js",
+      "cypress/e2e/2-advanced-examples/*.js",
+    ],
+    chromeWebSecurity: false,
+    experimentalSessionAndOrigin: true,
+    defaultCommandTimeout: 10000,
+    pageLoadTimeout: 120000,
   },
 });
