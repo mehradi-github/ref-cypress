86be31e6534f86131d6acc22feefb8679afdaae5
Mehradi <mehradi.info@gmail.com>
2024-01-10 09:36:54 +0100

    Adding screenshotOnRunFailure and trashAssetsBeforeRuns in cypress's config

--- cypress.config.js
+++ cypress.config.js
@@ -18,6 +18,8 @@ module.exports = defineConfig({
     defaultCommandTimeout: 10000,
     pageLoadTimeout: 150000,
     baseUrl: "https://webdriveruniversity.com",
+    screenshotOnRunFailure: true,
+    trashAssetsBeforeRuns: true,
     env: {
       first_name: "Sarah",
       homepage: "https://webdriveruniversity.com",
