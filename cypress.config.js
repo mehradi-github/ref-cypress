368dfac7aa410b075f388f55fd6a0ad56945f916
Mehradi <mehradi.info@gmail.com>
2024-01-17 09:17:01 +0100

    Loading custom config in cypress's config

--- cypress.config.js
+++ cypress.config.js
@@ -14,6 +14,8 @@ module.exports = defineConfig({
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
+      const file = config.env.configFile || "development";
+      return getConfig(file);
     },
     chromeWebSecurity: false,
     experimentalModifyObstructiveThirdPartyCode: true,
