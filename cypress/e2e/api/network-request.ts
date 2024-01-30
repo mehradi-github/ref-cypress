c1e99f93a17cb10aa0b35082d26ed238f8b6c86f
Mehradi <mehradi.info@gmail.com>
2024-01-30 20:59:42 +0100

    Put Request - Stubbing Server Response - XHR Testing

--- cypress/e2e/api/network-request.ts
+++ cypress/e2e/api/network-request.ts
@@ -1,6 +1,8 @@
 /// <reference types="cypress" />
 
 describe("Network Requests", () => {
+  let message = "Unable to find comment!";
+
   beforeEach(() => {
     cy.visit("https://example.cypress.io/commands/network-requests");
   });
@@ -27,7 +29,7 @@ describe("Network Requests", () => {
     cy.wait("@getComment").its("response.statusCode").should("eq", 200);
   });
 
-  it.only("Post Request", () => {
+  it("Post Request", () => {
     cy.intercept("POST", "/comments").as("postComment");
 
     cy.get(".network-post").click();
@@ -49,5 +51,25 @@ describe("Network Requests", () => {
         "https://example.cypress.io"
       );
     });
+  });
+
+  it.only("Put Request", () => {
+    cy.intercept(
+      {
+        method: "PUT",
+        url: "**/comments/*",
+      },
+      {
+        statusCode: 404,
+        body: { error: message },
+        delay: 500,
+      }
+    ).as("putComment");
+
+    cy.get(".network-put").click();
+
+    cy.wait("@putComment");
+
+    cy.get(".network-put-comment").should("contain", message);
   });
 });
