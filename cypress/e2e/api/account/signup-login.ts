e5cdbd7e6e7befc7b372c83c5e78eab9148f70c3
Mehradi <mehradi.info@gmail.com>
2024-01-31 09:11:03 +0100

    Automating Account Creation (Sign up)

---
+++ cypress/e2e/api/account/signup-login.ts
@@ -0,0 +1,18 @@
+/// <reference types="cypress" />
+
+describe("Signup & Login", () => {
+  let randomString = Math.random().toString(36).substring(2);
+
+  it("Test Valid Signup", () => {
+    //Repo: https://github.com/gothinkster/angular-realworld-example-app
+    cy.visit("https://angular.realworld.how/");
+
+    cy.get(".nav").contains("Sign up").click();
+    cy.get("[placeholder='Username']").type("Auto" + randomString);
+    cy.get("[placeholder='Email']").type(
+      "Auto_email" + randomString + "@gmail.com"
+    );
+    cy.get("[placeholder='Password']").type("Password1");
+    cy.get("button").contains("Sign up").click();
+  });
+});
