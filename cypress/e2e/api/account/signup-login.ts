6087637c94f3d6be1f612fe8eba795a97b377bbe
Mehradi <mehradi.info@gmail.com>
2024-01-31 09:19:39 +0100

    Test Valid Login

--- cypress/e2e/api/account/signup-login.ts
+++ cypress/e2e/api/account/signup-login.ts
@@ -4,6 +4,7 @@ describe("Signup & Login", () => {
   let randomString = Math.random().toString(36).substring(2);
   let username = "Auto" + randomString;
   let email = "Auto_email" + randomString + "@gmail.com";
+  let password = "Password1";
 
   it("Test Valid Signup", () => {
     cy.intercept("POST", "**/*.realworld.io/api/users").as("newUser");
@@ -25,5 +26,14 @@ describe("Signup & Login", () => {
       expect(request.body.user.username).to.eq(username);
       expect(request.body.user.email).to.eq(email);
     });
+  });
+
+  it.only("Test Valid Login", () => {
+    cy.visit("https://angular.realworld.how/");
+    cy.get(".nav").contains("Sign in").click();
+    cy.get("[placeholder='Email']").type(email);
+    cy.get("[placeholder='Password']").type(password);
+    cy.get("button").contains("Sign in").click();
+    cy.get(":nth-child(4) > .nav-link").contains(username);
   });
 });
