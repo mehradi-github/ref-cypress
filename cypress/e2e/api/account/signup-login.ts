d0c4e2f748faec3232e55c1943c69ebf78004910
Mehradi <mehradi.info@gmail.com>
2024-01-31 09:12:31 +0100

    UI & XHR Validation

--- cypress/e2e/api/account/signup-login.ts
+++ cypress/e2e/api/account/signup-login.ts
@@ -2,17 +2,28 @@
 
 describe("Signup & Login", () => {
   let randomString = Math.random().toString(36).substring(2);
+  let username = "Auto" + randomString;
+  let email = "Auto_email" + randomString + "@gmail.com";
 
   it("Test Valid Signup", () => {
+    cy.intercept("POST", "**/*.realworld.io/api/users").as("newUser");
+
     //Repo: https://github.com/gothinkster/angular-realworld-example-app
     cy.visit("https://angular.realworld.how/");
 
     cy.get(".nav").contains("Sign up").click();
-    cy.get("[placeholder='Username']").type("Auto" + randomString);
-    cy.get("[placeholder='Email']").type(
-      "Auto_email" + randomString + "@gmail.com"
-    );
+    cy.get("[placeholder='Username']").type(username);
+    cy.get("[placeholder='Email']").type(email);
     cy.get("[placeholder='Password']").type("Password1");
     cy.get("button").contains("Sign up").click();
+
+    cy.wait("@newUser").should(({ request, response }) => {
+      cy.log("Request: " + JSON.stringify(request));
+      cy.log("Response: " + JSON.stringify(response));
+
+      expect(response.statusCode).to.eq(200);
+      expect(request.body.user.username).to.eq(username);
+      expect(request.body.user.email).to.eq(email);
+    });
   });
 });
